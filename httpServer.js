6242f81cbb6426698090cad186d7dd53a5901242
Bryan Caldera <bryanicaldera@gmail.com>
2023-02-16 00:39:17 -0800

    adding https support

--- httpServer.js
+++ httpServer.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'portfolio', 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'portfolio', 'build', 'index.html'));
+});
+
 const port = process.env.PORT || 80;
 
 app.listen(port, () => {
---
+++ httpsServer.js
@@ -0,0 +1,18 @@
+//Gateway
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'portfolio', 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'portfolio', 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 443;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
