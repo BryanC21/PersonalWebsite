9d4e9abe2bbd10ca34f45983e0cc0e11fd93f6e6
Bryan Caldera <bryanicaldera@gmail.com>
2023-02-21 20:37:29 -0800

    test on new port

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3005;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
